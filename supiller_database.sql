a3ee033887ac8556e973e82a343029ec90fb9d88
sauravsinghcs24-lang <sauravsingh.cs24@bmsce.ac.in>
2025-11-27 09:52:06 +0530

    supiller_database.sql

--- supiller_database.sql
+++ supiller_database.sql
@@ -77,6 +77,99 @@ FROM Parts P, Suppliers S, Catalog C
 WHERE C.pid = P.pid AND C.sid = S.sid AND C.cost = (SELECT MAX(C1.cost)
 		                                            FROM Catalog C1
 		                                            WHERE C1.pid = P.pid);
+                                                    
+#extra querires												
+#1
+                                                
+SELECT P.pid, P.pname, S.sname, C.cost
+FROM Parts P, Suppliers S, Catalog C
+WHERE P.pid = C.pid AND S.sid = C.sid
+AND C.cost = (SELECT MAX(cost) FROM Catalog);
+
+
+#2
+
+SELECT S.sname
+FROM Suppliers S
+WHERE S.sid NOT IN (
+      SELECT C.sid
+      FROM Catalog C, Parts P
+      WHERE C.pid = P.pid AND P.color = 'Red');
+#3
+
+SELECT S.sname, SUM(C.cost) AS total_value
+FROM Suppliers S, Catalog C
+WHERE S.sid = C.sid
+GROUP BY S.sname;
+
+#4
+
+SELECT S.sname
+FROM Suppliers S, Catalog C
+WHERE S.sid = C.sid AND C.cost < 20
+GROUP BY S.sname
+HAVING COUNT(C.pid) >= 2;
+
+#5
+
+SELECT P.pname, S.sname, C.cost
+FROM Parts P, Suppliers S, Catalog C
+WHERE C.pid = P.pid AND C.sid = S.sid
+AND C.cost = (SELECT MIN(C1.cost)
+              FROM Catalog C1
+              WHERE C1.pid = P.pid);
+
+#6
+
+CREATE VIEW supplier_part_count AS
+SELECT s.sid, s.sname, COUNT(c.pid) AS total_parts
+FROM suppliers s
+LEFT JOIN catalog c ON s.sid = c.sid
+GROUP BY s.sid, s.sname;
+
+SELECT * FROM supplier_part_count;
+
+#7  
+        
+CREATE VIEW MostExpensiveSupplier AS
+SELECT P.pid, P.pname, S.sname, C.cost
+FROM PARTS P
+JOIN CATALOG C ON P.pid = C.pid
+JOIN SUPPLIERS S ON S.sid = C.sid
+WHERE C.cost = (
+      SELECT MAX(C1.cost)
+      FROM CATALOG C1
+      WHERE C1.pid = P.pid
+);
+SELECT*FROM MostExpensiveSupplier;
+
+              
+#8
+DELIMITER $$
+CREATE TRIGGER prevent_low_cost
+BEFORE INSERT ON catalog
+FOR EACH ROW
+BEGIN
+    IF NEW.cost < 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cost must be at least 1';
+    END IF;
+END$$
+DELIMITER ;
+
+
+#9
+
+DELIMITER $$
+CREATE TRIGGER set_default_cost
+BEFORE INSERT ON catalog
+FOR EACH ROW
+BEGIN
+    IF NEW.cost IS NULL THEN
+        SET NEW.cost = 15;  
+    END IF;
+END$$
+DELIMITER ;
 
 
 
